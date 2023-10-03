9a2b79c9560835f75dae3463e9e6d1fb88f1867b
Salt <elee823@gmail.com>
2023-10-03 13:04:40 -0400

    Added Basic JS vars and funcs

---
+++ script.js
@@ -0,0 +1,30 @@
+let prevVal = '';
+let newVal = '';
+let resultVal = '';
+let mathOp = '';
+
+//* store whether or not the decimal was clicked
+//* only allow one decimal per value
+let decimalClicked = false;
+
+//* hold values we want stored in memory
+let valMemStored = '';
+
+function numButPress(num) {
+
+}
+function mathButPress(operator) {
+    
+}
+function equalButPress() {
+    
+}
+function clearButPress() {
+    
+}
+function copyButPress() {
+    
+}
+function pasteButPress() {
+    
+}
